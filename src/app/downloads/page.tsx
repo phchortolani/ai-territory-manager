aafe6ba42f99dd4bd849687dbdcacb4b90dacae3
Paulo Henrique <phortolani@tmbeducacao.com.br>
2024-02-23 00:57:01 -0300

    feat: ajustes table

--- src/app/downloads/page.tsx
+++ src/app/downloads/page.tsx
@@ -8,42 +8,42 @@ export default async function DownloadPage() {
     return (<>
 
 
-        <div className="relative overflow-x-auto shadow-md sm:rounded-lg">
-            <table className="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
-                <caption className="p-5 text-lg font-semibold text-left rtl:text-right text-gray-900 bg-white dark:text-white dark:bg-gray-800">
+        <div className="relative overflow-x-auto  sm:rounded-lg">
+            <table className="w-full text-sm text-left rtl:text-right  text-gray-500 dark:text-gray-400">
+                <caption className="pb-2 text-lg font-semibold text-left rtl:text-right  bg-white  text-gray-900">
 
                     <SimpleButton typeBtn="primary">Download S13</SimpleButton>
                 </caption>
-                <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+                <thead className="text-xs border bg-gray-50  text-gray-900">
                     <tr>
-                        <th scope="col" className="px-6 py-3">
+                        <th scope="col" className="px-2 py-3 border">
                             Nº T.
                         </th>
-                        <th scope="col" className="px-6 py-3">
+                        <th scope="col" className="px-2 py-3 border">
                             Última data concluída
                         </th>
-                        <th scope="col" className="px-6 py-3">
+                        <th scope="col" className="px-2 py-3 border">
                             Data da designação
                         </th>
-                        <th scope="col" className="px-6 py-3">
+                        <th scope="col" className="px-2 py-3 border">
                             Data da conclusão
                         </th>
-                        <th scope="col" className="px-6 py-3">
+                        <th scope="col" className="px-2 py-3 border">
                             Data da designação
                         </th>
-                        <th scope="col" className="px-6 py-3">
+                        <th scope="col" className="px-2 py-3 border">
                             Data da conclusão
                         </th>
-                        <th scope="col" className="px-6 py-3">
+                        <th scope="col" className="px-2 py-3 border">
                             Data da designação
                         </th>
-                        <th scope="col" className="px-6 py-3">
+                        <th scope="col" className="px-2 py-3 border">
                             Data da conclusão
                         </th>
-                        <th scope="col" className="px-6 py-3">
+                        <th scope="col" className="px-2 py-3 border">
                             Data da designação
                         </th>
-                        <th scope="col" className="px-6 py-3">
+                        <th scope="col" className="px-2 py-3 border">
                             Data da conclusão
                         </th>
                     </tr>
@@ -53,57 +53,57 @@ export default async function DownloadPage() {
 
 
 
-                    {s13_list.map(x => {
+                    {s13_list.sort().map(x => {
                         return <>
-                            <tr key={x.id + "designado"} className="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
-                                <th rowSpan={2} scope="row" className="px-6 py-4 border text-center font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                            <tr key={x.id + "designado"} className="bg-white border-b text-gray-900 dark:border-gray-700">
+                                <th rowSpan={2} scope="row" className="px-2 p-1 border  text-sm text-center font-medium text-gray-900 whitespace-nowrap ">
                                     {x.id}
                                 </th>
 
-                                <td rowSpan={2} colSpan={1} className="px-6 py-4 border text-center">
+                                <td rowSpan={2} colSpan={1} className="px-2 p-1 border font-bold text-sm text-center">
                                     {moment(x.ultima_concluida).utc().format('DD/MM/YYYY')}
                                 </td>
-                                <td colSpan={2} className="px-6 py-4 border text-center ">
+                                <td colSpan={2} className="px-2 font-semibold p-1 border text-sm text-center ">
                                     {x.designado_1}
                                 </td>
-                                <td colSpan={2} className="px-6 py-4 border text-center">
+                                <td colSpan={2} className="px-2 p-1 font-semibold border text-sm text-center">
                                     {x.designado_2}
                                 </td>
-                                <td colSpan={2} className="px-6 py-4 border text-center">
+                                <td colSpan={2} className="px-2 p-1 font-semibold border text-sm text-center">
                                     {x.designado_3}
                                 </td>
 
-                                <td colSpan={2} className="px-6 py-4 border text-center">
-                                    {x.designado_4} 
+                                <td colSpan={2} className="px-2 p-1 font-semibold border text-sm text-center">
+                                    {x.designado_4}
                                 </td>
 
 
                             </tr>
-                            <tr key={x.id} className="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
+                            <tr key={x.id} className="bg-white border-b text-gray-900  dark:border-gray-700">
 
 
-                                <td className="px-6 py-4 border text-center">
+                                <td className="px-2 p-1 border text-sm text-center">
                                     {x.first_day_designado_em_1 && moment(x.first_day_designado_em_1).utc().format('DD/MM/YYYY')}
                                 </td>
-                                <td className="px-6 py-4 border text-center">
+                                <td className="px-2 p-1 border text-sm text-center">
                                     {x.last_day_designado_em_1 && moment(x.last_day_designado_em_1).utc().format('DD/MM/YYYY')}
                                 </td>
-                                <td className="px-6 py-4 border text-center">
+                                <td className="px-2 p-1 border text-sm text-center">
                                     {x.first_day_designado_em_2 && moment(x.first_day_designado_em_2).utc().format('DD/MM/YYYY')}
                                 </td>
-                                <td className="px-6 py-4 border text-center">
+                                <td className="px-2 p-1 border text-sm text-center">
                                     {x.last_day_designado_em_2 && moment(x.last_day_designado_em_2).utc().format('DD/MM/YYYY')}
                                 </td>
-                                <td className="px-6 py-4 border text-center">
+                                <td className="px-2 p-1 border text-sm text-center">
                                     {x.first_day_designado_em_3 && moment(x.first_day_designado_em_3).utc().format('DD/MM/YYYY')}
                                 </td>
-                                <td className="px-6 py-4 border text-center">
+                                <td className="px-2 p-1 border text-sm text-center">
                                     {x.last_day_designado_em_3 && moment(x.last_day_designado_em_3).utc().format('DD/MM/YYYY')}
                                 </td>
-                                <td className="px-6 py-4 border text-center">
+                                <td className="px-2 p-1 border text-sm text-center">
                                     {x.first_day_designado_em_4 && moment(x.first_day_designado_em_4).utc().format('DD/MM/YYYY')}
                                 </td>
-                                <td className="px-6 py-4 border text-center">
+                                <td className="px-2 p-1 border text-sm text-center">
                                     {x.last_day_designado_em_4 && moment(x.last_day_designado_em_4).utc().format('DD/MM/YYYY')}
                                 </td>
                             </tr>
